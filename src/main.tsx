181b9f925c8a8eef86fbe78b52c870070524827d
Manuel Garcia <33798495+fullsnack-coder@users.noreply.github.com>
2022-05-06 17:01:21 -0500

    chore: update theme colors (#10)

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,17 @@
+import { ThemeProvider } from "@mui/material"
 import React from "react"
 import ReactDOM from "react-dom/client"
 import App from "./App"
 import "./index.css"
 import { StoreProvider } from "./store"
+import appTheme from "./utils/theme"
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <StoreProvider>
-      <App />
-    </StoreProvider>
+    <ThemeProvider theme={appTheme}>
+      <StoreProvider>
+        <App />
+      </StoreProvider>
+    </ThemeProvider>
   </React.StrictMode>
 )
---
+++ src/utils/theme.ts
@@ -0,0 +1,14 @@
+import { createTheme } from "@mui/material";
+
+const appTheme = createTheme({
+  palette: {
+    primary: {
+        main: "#65a30d"
+    },
+    secondary: {
+        main: '#222222'
+    }
+  },
+});
+
+export default appTheme;
